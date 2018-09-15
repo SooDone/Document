c94ed377c5cb1fe19b2de0c80187016ff03b9bf2
SooDone <42586542+SooDone@users.noreply.github.com>
2018-09-14 23:33:55 -0500

    Add files via upload

---
+++ document.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Document1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                //Pass the filepath and filename to the StreamWriter Constructor
+                StreamWriter sw = new StreamWriter("C:\\Test.txt");
+
+                //Write a line of text
+                sw.WriteLine("Hello World!!");
+
+                //Write a second line of text
+                sw.WriteLine("From the StreamWriter class");
+
+                //Close the file
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+
+        }
+    }
+}
